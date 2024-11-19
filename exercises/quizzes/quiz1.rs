6ae474a713ec84e141336666996b56d39f021dd2
Gerardo Puga <glpuga@gmail.com>
2024-11-19 20:27:34 -0300

    More exercises

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -8,9 +8,13 @@
 // - However, if Mary buys more than 40 apples, the price of each apple in the
 // entire order is reduced to only 1 rustbuck!
 
-// TODO: Write a function that calculates the price of an order of apples given
-// the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(price : u32) -> u32 {
+    if price <= 40 {
+        price * 2
+    } else {
+        price
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
