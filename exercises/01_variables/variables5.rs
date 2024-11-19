6ef9782f6ecb83a906d3fa35324f03cdd86bf741
Gerardo Puga <glpuga@gmail.com>
2024-11-18 21:34:48 -0300

    Solve rustlings

---
+++ exercises/01_variables/variables5.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let number = "T-H-R-E-E"; // Don't change this line
+    println!("Spell a number: {}", number);
+
+    {
+        let number : u32 = 3;
+        println!("Number plus two is: {}", number + 2);
+    }
+}
